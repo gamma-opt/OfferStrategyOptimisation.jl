5611202629150ec890cec08064878f623ec99423
Helmi Hankimaa <helmi.hankimaa@aalto.fi>
2023-05-08 17:17:34 -0300

    Variables for running model without ID market.

---
+++ variables_no_ID.jl
@@ -0,0 +1,43 @@
+using JuMP
+
+## -- VARIABLES --  
+
+# DA offer quantities
+@variable(model, x[I, T] ≥ 0)
+
+# DA dispatch quantities
+@variable(model, y[T, S] ≥ 0)
+
+# Reserve offer quantities
+@variable(model, v[J, T] ≥ 0)
+
+# Reserve offer quantities
+@variable(model, r[T, S] ≥ 0)
+
+# ID order (trade) quantities
+@variable(model, z[T, S, E] == 0) # <=
+
+# Imbalances
+@variable(model, delta_excess[T, S, E, W] ≥ 0)
+@variable(model, delta_deficit[T, S, E, W] ≥ 0)
+
+
+# -- Hydro power generation --
+@variable(model, g_hydro[T, S, E] ≥ 0)
+@variable(model, u_hydro[T, S, E], Bin)
+@variable(model, u_hydro_start[T, S, E], Bin)
+@variable(model, u_hydro_stop[T, S, E], Bin)
+@variable(model, f_hydro[T, S, E] ≥ 0)
+@variable(model, f_hydro_spill[T, S, E] ≥ 0)
+@variable(model, l_hydro[T, S, E] ≥ 0)
+@variable(model, r_hydro[T, S, E] ≥ 0)
+
+# -- Conventional generation --
+@variable(model, g_CCGT[T, S, E] ≥ 0)
+@variable(model, u_CCGT[T, S, E], Bin)
+@variable(model, u_CCGT_start[T, S, E], Bin)
+@variable(model, u_CCGT_stop[T, S, E], Bin)
+@variable(model, r_CCGT[T, S, E] ≥ 0)
+
+# -- Wind generation --
+@variable(model, g_wind[T, S, E, W] ≥ 0)
