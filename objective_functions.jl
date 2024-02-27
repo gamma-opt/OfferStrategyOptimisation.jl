d14f14c921d94776bd195f8753549c76c1c5110b
helmihankimaa <helmi.hankimaa@aalto.fi>
2024-02-27 14:41:07 +0200

    Added structures for problem sets and parameters.

--- objective_functions.jl
+++ objective_functions.jl
@@ -2,22 +2,30 @@ using JuMP
 
 ## -- OBJECTIVE --
 
-function objective!(model; DA_revenue::Bool=true, reserve_revenue::Bool=true, 
+function objective!(model::Model, sets::Sets, prices::Prices, costs::Costs; 
+    DA_revenue::Bool=true, reserve_revenue::Bool=true, 
     ID_revenue::Bool=true, imbalance_revenue::Bool=true, CCGT_costs::Bool=true, 
     hydro_costs::Bool=true, WVF_piecewise::Bool=true, return_expression::Bool=false)
+    T = sets.T
+    S = sets.S
+    E = sets.E
+    W = sets.W
+    π_S = sets.π_S
+    π_E = sets.π_E
+    π_W = sets.π_W
 
-    DA = DA_revenue ? @expression(model, sum(π_S[s] * DA_prices[t,s] * y[t,s] for t in T, s in S)) : 0
+    DA = DA_revenue ? @expression(model, sum(π_S[s] * prices.DA[t,s] * y[t,s] for t in T, s in S)) : 0
 
-    reserve = reserve_revenue ? @expression(model,sum(π_S[s] * reserve_prices[t,s] * r[t,s] for t in T, s in S )) : 0
+    reserve = reserve_revenue ? @expression(model,sum(π_S[s] * prices.reserve[t,s] * r[t,s] for t in T, s in S )) : 0
 
-    ID = ID_revenue ? @expression(model, sum(π_S[s] * π_E[e] * ID_prices[t, s, e] * z[t,s,e] for t in T, s in S, e in E)) : 0
+    ID = ID_revenue ? @expression(model, sum(π_S[s] * π_E[e] * prices.ID[t, s, e] * z[t,s,e] for t in T, s in S, e in E)) : 0
 
-    imbalance = imbalance_revenue ? @expression(model, sum( π_S[s] * π_E[e] * π_W[w] * (balance_prices_down[t,s,e,w] * delta_excess[t,s,e,w] - balance_prices_up[t,s,e,w] * delta_deficit[t,s,e,w]) for t in T, s in S, e in E, w in W )) : 0
+    imbalance = imbalance_revenue ? @expression(model, sum( π_S[s] * π_E[e] * π_W[w] * (prices.down[t,s,e,w] * delta_excess[t,s,e,w] - prices.up[t,s,e,w] * delta_deficit[t,s,e,w]) for t in T, s in S, e in E, w in W )) : 0
 
 
-    CCGT = CCGT_costs ? @expression(model, sum( π_S[s] * π_E[e] * ( S_CCGT_generation * g_CCGT[t,s,e] 
-                            + S_CCGT_startup * u_CCGT_start[t,s,e] 
-                            + S_CCGT_shutdown * u_CCGT_stop[t,s,e])
+    CCGT = CCGT_costs ? @expression(model, sum( π_S[s] * π_E[e] * ( costs.CCGT_generation * g_CCGT[t,s,e] 
+                            + costs.CCGT_startup * u_CCGT_start[t,s,e] 
+                            + costs.CCGT_shutdown * u_CCGT_stop[t,s,e])
                             for t in T, s in S, e in E)) : 0
 
     # Option of piecewise WVF or simple marginal cost in eur/MWh
@@ -25,16 +33,16 @@ function objective!(model; DA_revenue::Bool=true, reserve_revenue::Bool=true,
 
         println("\n\n\n******************************\n\n WVF piecewise code not tested!!\n\n******************************")
 
-        hydro_startup = @expression(model, sum( π_S[s] * π_E[e] * S_hydro_startup * u_hydro_start[t,s,e] for t in T, s in S, e in E))
+        hydro_startup = @expression(model, sum( π_S[s] * π_E[e] * costs.Hydro_startup * u_hydro_start[t,s,e] for t in T, s in S, e in E))
 
-        hydro_opportunity_cost = @expression(model, - sum( π_S[s] * π_E[e] * (B_hydro_k[k, 1] * l_hydro_k[t, s, e, k] + B_hydro_k[k, 2])
-                            for t = nT, s in S, e in E, k in K))
+        hydro_opportunity_cost = @expression(model, - sum( π_S[s] * π_E[e] * (costs.Hydro_WV[k, 1] * l_hydro_k[t, s, e, k] + costs.Hydro_WV[k, 2])
+                            for t = T[end], s in S, e in E, k in K))
 
     elseif hydro_costs && !WVF_piecewise
 
-        hydro_startup = @expression(model, sum( π_S[s] * π_E[e] * S_hydro_startup * u_hydro_start[t,s,e] for t in T, s in S, e in E))
+        hydro_startup = @expression(model, sum( π_S[s] * π_E[e] * costs.Hydro_startup * u_hydro_start[t,s,e] for t in T, s in S, e in E))
 
-        hydro_opportunity_cost = @expression(model, - sum( π_S[s] * π_E[e] * (B_hydro[1] * l_hydro[t, s, e] + B_hydro[2]) for t = nT, s in S, e in E))
+        hydro_opportunity_cost = @expression(model, - sum( π_S[s] * π_E[e] * (costs.Hydro_WV[1] * l_hydro[t, s, e] + costs.Hydro_WV[2]) for t = T[end], s in S, e in E))
     else
         hydro_startup = 0
         hydro_opportunity_cost = 0
@@ -51,8 +59,13 @@ end
 
 
 ## -- OBJECTIVE FOR DEGENERATE SOLUTIONS --
-function objective_degenerate_solutions!(model::Model, delta_excess, delta_deficit, T, S, E, W)
+function objective_degenerate_solutions!(model::Model, sets::Sets, delta_excess, delta_deficit)
     
+    T = sets.T
+    S = sets.S
+    E = sets.E
+    W = sets.W
+
     @objective(model, Min, sum(delta_excess[t,s,e,w] + delta_deficit[t,s,e,w] for t in T, s in S, e in E, w in W))
 
 end
--- variables.jl
+++ variables.jl
@@ -1,19 +1,23 @@
 using JuMP
 
-## -- VARIABLES --  
+## -- Market variables --  
 
 "Declare variables x, y, v, r, z, Δ⁺, Δ⁻ related to market functions.
 
 If variables for ID or reserve market are not included in the model, the function returns zero matrices for them."
 function market_variables(model::Model, 
-    T::UnitRange{Int64}, 
-    S::UnitRange{Int64}, 
-    E::UnitRange{Int64}, 
-    W::UnitRange{Int64}, 
-    I::UnitRange{Int64}, 
-    J::UnitRange{Int64};
-    include_Reserve::Bool = true,
-    include_ID::Bool = true)
+                        sets::Sets;
+                        include_Reserve::Bool = true,
+                        include_ID::Bool = true)
+
+    # Readability
+    T = sets.T
+    S = sets.S
+    E = sets.E
+    W = sets.W
+    I = sets.I
+    J = sets.J
+
 
     # DA offer quantities
     @variable(model, x[I, T] ≥ 0)
@@ -47,17 +51,23 @@ function market_variables(model::Model,
 end
 
 
+
 # -- Hydro power generation --
 "Declare hydropower generation related variables g_hydro, u_hydro, u_hydro_start, u_hydro_stop, f_hydro, f_hydro_spill, l_hydro, r_hydro.
 
 If variables for reserve market or on-off functionality are not included in the model, the function returns zero or one matrices for them.
 "
 function hydropower_variables(model::Model, 
-    T::UnitRange{Int64}, 
-    S::UnitRange{Int64}, 
-    E::UnitRange{Int64};
+    sets::Sets;
     include_Reserve::Bool = true,
     include_on_off_functionality = true)
+
+
+    # Readability
+    T = sets.T
+    S = sets.S
+    E = sets.E
+
 
     @variable(model, g_hydro[T, S, E] ≥ 0)
 
@@ -86,15 +96,20 @@ function hydropower_variables(model::Model,
 
 end
 
+
+
 # -- CCGT generation --
 "Declare CCGT generation related variables g_CCGT, u_CCGT, u_CCGT_start, u_CCGT_stop, r_CCGT.
 
 If variables for reserve market are not included in the model, the function returns zero matrices for them."
 function CCGT_generation_variables(model::Model, 
-    T::UnitRange{Int64}, 
-    S::UnitRange{Int64}, 
-    E::UnitRange{Int64};
+    sets::Sets;
     include_Reserve::Bool = true)
+
+    # Readability
+    T = sets.T
+    S = sets.S
+    E = sets.E
 
     @variable(model, g_CCGT[T, S, E] ≥ 0)
     @variable(model, u_CCGT[T, S, E], Bin)
@@ -110,13 +125,17 @@ function CCGT_generation_variables(model::Model,
     return g_CCGT, u_CCGT, u_CCGT_start, u_CCGT_stop, r_CCGT
 end
 
+
+
 # -- Wind generation --
 "Declare wind generation related variables g_wind."
-function wind_generation_variables(model::Model,
-    T::UnitRange{Int64}, 
-    S::UnitRange{Int64}, 
-    E::UnitRange{Int64},
-    W::UnitRange{Int64})
+function wind_generation_variables(model::Model, sets::Sets)
+
+    # Readability
+    T = sets.T
+    S = sets.S
+    E = sets.E
+    W = sets.W
     
     @variable(model, g_wind[T, S, E, W] ≥ 0)
 
